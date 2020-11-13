3fe9573b9e06edfac716fecc6b837b52c6c2151e
Toktam <ttmohebbi@gmail.com>
2020-11-13 17:54:29 +0000

    exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,17 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |elem|
+  puts "The store of #{elem.name} had an annual revenue of #{elem.annual_revenue}."
+end
+
+# careful when using conditions to not allow SQL injections. Use Array conditions instead of pure string conditions
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+@womens_stores.each do |elem|
+  puts "The store of #{elem.name} had an annual revenue of #{elem.annual_revenue}."
+end
